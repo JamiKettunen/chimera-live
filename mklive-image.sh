ff9a5905c13c2dfa38518eb58b54b4e9c6e72629
q66 <q66@chimera-linux.org>
2026-07-20 14:18:37 +0200

    mklive-imge: drop xorg from plasma images

--- mklive-image.sh
+++ mklive-image.sh
@@ -44,7 +44,7 @@ case "$IMAGE" in
         PKGS="${BASE_PKGS} gnome"
         ;;
     plasma)
-        PKGS="${BASE_PKGS} plasma-desktop xserver-xorg"
+        PKGS="${BASE_PKGS} plasma-desktop"
         ;;
     *)
         echo "unknown image type: $IMAGE"
